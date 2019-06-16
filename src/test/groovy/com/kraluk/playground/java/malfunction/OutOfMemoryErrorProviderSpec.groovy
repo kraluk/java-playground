9fbc721934836a6d5beabbefeb59f100b559583e
Łukasz Krauzowicz <kraluk@gmail.com>
2019-06-16 16:14:14 +0200

    Ignored OOME related test.

--- src/test/groovy/com/kraluk/playground/java/malfunction/OutOfMemoryErrorProviderSpec.groovy
+++ src/test/groovy/com/kraluk/playground/java/malfunction/OutOfMemoryErrorProviderSpec.groovy
@@ -1,7 +1,9 @@
 package com.kraluk.playground.java.malfunction
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
+@Ignore("Just for experiments")
 class OutOfMemoryErrorProviderSpec extends Specification {
 
     def "should provide an OutOfMemoryError"() {
